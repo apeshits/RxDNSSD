6e42a8fbe3813db1191f5d0a6f186312cdf3517a
Apeshits <1529168905@qq.com>
2024-01-23 17:28:48 +0800

    explain some function

--- app/src/main/java/com/github/druk/dnssdsamples/MainActivity.java
+++ app/src/main/java/com/github/druk/dnssdsamples/MainActivity.java
@@ -28,7 +28,6 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
     private Rx2Dnssd rxDnssd;
-
     @Nullable
     private Disposable browseDisposable;
     @Nullable
@@ -45,17 +44,21 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.check_threads).setOnClickListener(v -> {
             /*
-             *   When make browse after all services were found and timeout exhausted (default 60 sec) should be only 5 threads:
+             *   当我们在执行browse操作时，当所有的服务都被发现并且超时时间耗尽（默认60秒）时，应该只有5个线程在活动：
              *   - main
              *   - NsdManager
              *   - Thread #<n> (it's DNSSD browse thread)
              *   - RxIoScheduler-1 (rx possibly can create more or less threads, in my case was 2)
              *   - RxIoScheduler-2
              */
+
+            // 记录当前活动线程的数量
             Log.i("Thread", "Thread count " + Thread.activeCount() + ":");
+
+            // 获取所有活动线程的集合
             Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
             for (Thread thread : threadSet) {
-                // We only interested in main group
+                // We only interested in main group（主线程组）
                 if (Objects.requireNonNull(thread.getThreadGroup()).getName().equals("main")) {
                     Log.v("Thread", thread.getName());
                 }
@@ -114,12 +117,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void startBrowse() {
         Log.i("TAG", "start browse");
+        // 使用Rx2Dnssd提供的browse()方法来开始Bonjour服务的浏览。这是一个异步操作，使用RxJava的Observable来处理结果。
         browseDisposable = rxDnssd.browse("_rxdnssd._tcp", "local.")
-                .compose(rxDnssd.resolve())
-                .compose(rxDnssd.queryIPRecords())
+                .compose(rxDnssd.resolve())                 // 使用resolve()方法来解析Bonjour服务的IP地址和端口号
+                .compose(rxDnssd.queryIPRecords())          // 使用queryIPRecords()方法来查询Bonjour服务的IP地址
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(bonjourService -> {
+                .subscribe(bonjourService -> {              // 订阅浏览操作的结果，包括成功时的回调(bonjourService)和错误时的回调(throwable)。
                     Log.d("TAG", bonjourService.toString());
                     if (bonjourService.isLost()) {
                         mServiceAdapter.remove(bonjourService);
@@ -141,10 +145,11 @@ public class MainActivity extends AppCompatActivity {
         Log.i("TAG", "register");
         button.setEnabled(false);
         BonjourService bs = new BonjourService.Builder(0, 0, Build.DEVICE, "_rxdnssd._tcp", null).port(123).build();
+        // 异步操作，使用Rx2Dnssd提供的register方法来注册Bonjour服务
         registerDisposable = rxDnssd.register(bs)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(bonjourService -> {
+                .subscribeOn(Schedulers.io())               // 指定注册操作在 IO 线程执行，以防止在主线程上执行耗时的网络操作。
+                .observeOn(AndroidSchedulers.mainThread())  // 指定结果的处理发生在主线程，以便在 UI 上更新界面。
+                .subscribe(bonjourService -> {              // 订阅注册操作的结果，包括成功时的回调(bonjourService)和错误时的回调(throwable)。
                     Log.i("TAG", "Register successfully " + bonjourService.toString());
                     button.setEnabled(true);
                     button.setText(R.string.unregister);
